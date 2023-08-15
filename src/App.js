e516f7d652ba273c3f5dd1748b7a4d79b6fea2a4
Geoff Lloyd <orionblack@gmail.com>
2023-08-15 10:21:51 -0700

    initializing files, trying to push to make sure everything is not broken

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './Header';
+import Gallery from './Gallery';
+import Footer from './Footer';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p>Stuf</p>
+        <Header />
+        <Gallery />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import { render } from 'react-dom';
+
+class Header extends React.Component {};
+render(
+  <>
+  <h1>Gallery of Horns</h1>
+  </>
+);
+export default Header;
