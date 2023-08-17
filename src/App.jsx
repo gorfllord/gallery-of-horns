8a3f253dbd62d86384e41e26e2fa11ea84eee0a8
Geoff Lloyd <orionblack@gmail.com>
2023-08-16 18:26:48 -0700

    importing from json file now, added bootstrap and pizzazz

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
 import './App.css';
 import Header from './Header';
 import Gallery from './Gallery';
 import Footer from './Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <Header />
-        
         <Gallery />
         <Footer />
       </div>
